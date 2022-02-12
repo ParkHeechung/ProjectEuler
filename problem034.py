2f8276fb0c14e8d6548fdf303d3d8f6c8c86a8e6
ParkHeechung <99527002+ParkHeechung@users.noreply.github.com>
2022-02-12 15:44:34 +0900

    Create problem034.py

---
+++ problem034.py
@@ -0,0 +1,18 @@
+def get_factorial(number):
+    factorial = 1
+    for n in range(1, number + 1):
+        factorial *= n
+    return factorial
+
+def get_cipher_factorial_sum(number):
+    cipher_factorial_sum = 0
+    for i in str(number):
+        cipher_factorial_sum += get_factorial(int(i))
+    return cipher_factorial_sum
+
+answer = 0
+for number in range(1, 1000000):
+    cipher_factorial_sum = get_cipher_factorial_sum(number)
+    if cipher_factorial_sum == number:
+        answer += number
+    print(answer - 3)
