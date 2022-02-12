4e7040af87bb49eaefecccc71c7fcd7ac5dcd507
ParkHeechung <99527002+ParkHeechung@users.noreply.github.com>
2022-02-12 15:40:25 +0900

    Update problem036.py

--- problem036.py
+++ problem036.py
@@ -1,5 +1,5 @@
 answer = 0
-for number in range(1, 1000000):
+for number in range(1, 1000):
     if str(number) == str(number)[::-1]:
         if str(bin(number)[2:]) == str(bin(number)[2:][::-1]):
             answer += number
