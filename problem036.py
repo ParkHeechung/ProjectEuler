6aea51c557a17df8e5657399db77ada0006855f1
ParkHeechung <99527002+ParkHeechung@users.noreply.github.com>
2022-02-12 15:43:35 +0900

    Update problem036.py

--- problem036.py
+++ problem036.py
@@ -1,6 +1,6 @@
 answer = 0
-for number in range(1, 1000):
+for number in range(1, 1000000):
     if str(number) == str(number)[::-1]:
-        if str(bin(number)[4:]) == str(bin(number)[2:][::-1]):
+        if str(bin(number)[2:]) == str(bin(number)[2:][::-1]):
             answer += number
         print(answer)
