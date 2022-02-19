6ab2145dcaac6aa6b3f495777ef8f2e5838b7c16
ParkHeechung <99527002+ParkHeechung@users.noreply.github.com>
2022-02-19 15:38:02 +0900

    Update problem039.py

--- problem039.py
+++ problem039.py
@@ -1,14 +1,22 @@
-for p in range(3, 1000):
-    p_count = 0
-    for c in range(1, 333):
-        for a in range(1, c):
-            b = p - c - a
-            if b == (c**2 - a**2)**0.5:
-                p_count += 1
-            if c <= b:
-                break
-            
-max_p_count = 0
-if max_p_count < p_count:
-   p_count = max_p_count
-print(max_p_count)
+from collections import Counter
+
+counter = Counter()
+triangles = []
+for a in range(1, 500):
+    for b in range(1, 500):
+        c = (a**2 + b**2)**0.5
+        if int(c) == c:
+            if a + b + c <= 1000:
+                counter[a + b + c] += 1
+most_common = counter.most_common(1)
+answer = most_common[0][0]
+                     
+                 
+# from collections import Counter
+
+# counter = Counter()
+# triangles = [10, 12, 12, 14, 16, 18, 20, 14, 14, 10]
+# for p in triangles:
+#     counter[p] += 1
+# most_common = counter.most_common(1)
+# answer = most_common[0][0]
