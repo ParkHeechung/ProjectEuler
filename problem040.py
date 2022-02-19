6091d5ab796c04a43b56b2111a262488f667bdb7
ParkHeechung <99527002+ParkHeechung@users.noreply.github.com>
2022-02-19 16:11:30 +0900

    Update problem040.py

--- problem040.py
+++ problem040.py
@@ -2,3 +2,9 @@ champernownes_constant = "0."
 for number in range(1, 10**6):
     champernownes_constant += str(number)
 answer = int(champernownes_constant[2]) * int(champernownes_constant[11]) * int(champernownes_constant[101]) * int(champernownes_constant[1001]) * int(champernownes_constant[10001]) * int(champernownes_constant[100001]) * int(champernownes_constant[1000001]) 
+
+####
+answer = 1
+for n in range(7):
+    d_n = champernownes_constant[10**n + 1]
+    answer *= int(d_n)
