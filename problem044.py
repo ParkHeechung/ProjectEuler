314ab5b44aaeec3f5ecf780a865385f30fd7b597
ParkHeechung <99527002+ParkHeechung@users.noreply.github.com>
2022-02-26 16:00:15 +0900

    Update problem044.py

--- problem044.py
+++ problem044.py
@@ -1,17 +1,17 @@
 def get_pentagonal_number(number):
-    pentagonal_number_set = set()
+    pentagonal_numbers = []
     for n in range(1, number +1):
-        pentagonal_number = n * (n * 3 - 1) / 2
-        pentagonal_number_set.add(pentagonal_number)
-    return pentagonal_number_set
+        pentagonal_number = n * (n * 3 - 1) // 2
+        pentagonal_numbers.append(pentagonal_number)
+    return pentagonal_numbers
 
 
 smallest_answer = float(10000000)
-pentagonal_number_set = get_pentagonal_number(1000)
-for k in range(1000):
-    for j in range(1000):
-        if k * (k * 3 - 1) / 2 + j * (j * 3 - 1) / 2 in pentagonal_number_set:
-            if k * (k * 3 - 1) / 2 - j * (j * 3 - 1) / 2 in pentagonal_number_set:
-                if k * (k * 3 - 1) / 2 - j * (j * 3 - 1) / 2 < smallest_answer:
-                    smallest_answer = k * (k * 3 - 1) / 2 - j * (j * 3 - 1) / 2
-print(smallest_answer)
+pentagonal_numbers = get_pentagonal_number(10000)
+pentagonal_number_set = set(pentagonal_numbers)
+for k in range(10000):
+    for j in range(10000):
+        p_j = pentagonal_numbers[j]
+        p_k = pentagonal_numbers[k]
+        if p_j + p_k in pentagonal_number_set and p_j - p_k in pentagonal_number_set:
+            print(p_j - p_k)
